083106b4842a4b20fbd50ccda8fee823e61ba46f
Elena.M <el.muss@yandex.ru>
2024-11-17 15:34:11 +0300

    Диплом ТЗ1 версия 1

--- stats/stats-server/src/main/java/org/example/repository/StatsRepository.java
+++ stats/stats-server/src/main/java/org/example/repository/StatsRepository.java
@@ -11,12 +11,14 @@ public interface StatsRepository extends JpaRepository<Stats, Integer> {
     @Query(value = "select s from Stats s where s.uri like (?1) and s.timestamp between ?2 and ?3")
     List<Stats> findByUriAndTimestamp(String uri, Instant start, Instant end);
 
-    @Query(value = "select s from Stats s where s.uri like (?1) and s.timestamp between ?2 and ?3")
+    @Query(value = "select s from Stats s where s.ip in (select distinct s.ip from Stats s) and s.uri like (?1)" +
+            "and s.timestamp between ?2 and ?3 group by s.id")
     List<Stats> findByUriAndTimestampUnique(String uri, Instant start, Instant end);
 
     @Query(value = "select s from Stats s where s.timestamp between ?1 and ?2")
     List<Stats> findAllByTimestamp(Instant start, Instant end);
 
-    @Query(value = "select s from Stats s where s.timestamp between ?1 and ?2")
+    @Query(value = "select s from Stats s where s.ip in (select distinct s.ip from Stats s) " +
+            "and s.timestamp between ?1 and ?2 group by s.id")
     List<Stats> findAllByTimestampUnique(Instant start, Instant end);
 }
