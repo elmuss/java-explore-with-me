7cf9731a2e47fc1421ae79219a9d18e36c47c939
Elena.M <el.muss@yandex.ru>
2024-12-15 15:14:36 +0300

    Диплом ТЗ2 версия 1

--- stats/stats-client/src/main/java/org/example/StatsHitClient.java
+++ stats/stats-client/src/main/java/org/example/StatsHitClient.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
-import org.springframework.stereotype.Component;
 import org.springframework.web.util.DefaultUriBuilderFactory;
 
 
